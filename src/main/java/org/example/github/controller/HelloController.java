081c228ddd6e00b74703fe09bd581dc5ada59a61
fengyunlong <fyl19920820@163.com>
2022-09-22 14:32:40 +0800

    测试自动构建

--- src/main/java/org/example/github/controller/HelloController.java
+++ src/main/java/org/example/github/controller/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
     }
     @GetMapping("hello2")
     public String hello2() {
-        System.out.println("hello22");
+        System.out.println("hello2");
         return "hello2";
     }
 }
